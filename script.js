0033f35df62f3f4f22059beda30f730c26238891
doronddav <dorond300@gmail.com>
2022-04-25 11:20:45 +0300

    Added arguments to Pokemon object

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ class Pokemon {
   //     this.types = types;
   //   }
 
-  constructor(name) {
+  constructor(name, image, moves, types) {
     this.name = name;
-    // this.image = image;
+    this.image = image;
+    this.moves = moves;
+    this.types = types;
   }
-
-  //   static get name() {
-  //     return this.name;
-  //   }
 }
 
 let pokeInputName = document.getElementById("pokemonName");
@@ -23,24 +21,51 @@ let pokeId = document.getElementById("pokemonId");
 let search = document.getElementById("search");
 let pokemonName = document.getElementById("pokemonName");
 let pokemonImg = document.getElementById("pokemonImg");
+let movesLi = document.getElementById("moves");
+let typesLi = document.getElementById("type");
 let pokemon;
 
 const num = "";
-const pokemonApi = `https://pokeapi.co/api/v2/pokemon/`;
+const pokemonApi = `https://pokeapi.co/api/v2/pokemon?limit=1126/`;
 
 fetch(`${pokemonApi}`)
   .then((response) => response.json())
-  .then((response) => fetch(response.results[10].url))
+  .then((response) => fetch(response.results[20].url))
 
   .then((response) => response.json())
   // .then((response) => console.log(response))
   .then(
-    (response) => (pokemon = new Pokemon(response.name, response.species.url))
+    (response) =>
+      (pokemon = new Pokemon(
+        response.name,
+        response.sprites.front_default,
+        response.moves,
+        response.types
+      ))
   )
-  .then((pokemon) => console.log(pokemon.name))
+  // .then((pokemon) => console.log(pokemon.name));
+  .then((pokemon) => console.log(pokemon.types[0].type.name))
   .finally(() => displayPokemon());
 
 let displayPokemon = function () {
   pokemonName.innerHTML = pokemon.name;
   pokemonImg.src = pokemon.image;
+  displayMoves(pokemon.moves);
+  displayType(pokemon.types);
+};
+
+let displayMoves = function (moves) {
+  for (let i = 0; i < moves.length; i++) {
+    let moveElement = document.createElement("li");
+    moveElement.innerHTML = moves[i].move.name;
+    movesLi.appendChild(moveElement);
+  }
+};
+
+let displayType = function (types) {
+  for (let i = 0; i < types.length; i++) {
+    let typeElement = document.createElement("li");
+    typeElement.innerHTML = types[i].type.name;
+    typesLi.appendChild(typeElement);
+  }
 };
